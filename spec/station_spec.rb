5f98236baf27e33405b3426f97720f574a2245eb
ewa <ewa.jablonskaa@gmail.com>
2021-09-28 17:00:13 +0100

    step 13 - creation of station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,19 @@
+require 'station'
+
+describe Station do
+  
+  let(:station) { Station.new("Victoria", 1) }
+
+  describe "#initialize" do
+    it "has a name" do
+      expect(station.name).to eq "Victoria"
+    end
+
+    it "has a zone" do
+      expect(station.zone).to eq 1
+    end
+
+  end
+  #it {is_expected.to respond_to(:name)}
+
+end
